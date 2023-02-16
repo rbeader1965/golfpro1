90bea3f2839ccdd944755d42d9ce094c50b03bab
RAY <raybeader@hotmail.com>
2023-02-16 10:42:00 -0800

    commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,5 @@
-<h1>Welcome to Golfpro 1</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis quas animi culpa optio repudiandae, nemo sit deserunt illum, nostrum eaque explicabo, illo tempora sint error!</p>
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  
+ 
